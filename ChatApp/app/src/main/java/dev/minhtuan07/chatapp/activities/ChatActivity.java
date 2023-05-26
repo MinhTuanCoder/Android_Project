c5787246068e9b713286a8f3655f74def34ce6e9
Nguyễn Minh Tuấn <86965221+MinhTuanCoder@users.noreply.github.com>
2023-05-26 15:38:04 +0700

    #7 | Chat Screen Layout Design
    Thiết kế các file giao diện xml cho phần chat

---
+++ ChatApp/app/src/main/java/dev/minhtuan07/chatapp/activities/ChatActivity.java
@@ -0,0 +1,16 @@
+package dev.minhtuan07.chatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dev.minhtuan07.chatapp.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
