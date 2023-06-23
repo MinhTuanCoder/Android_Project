1fe705eacdd33154d016217aa1a1cc706073bb29
Nguyễn Minh Tuấn <86965221+MinhTuanCoder@users.noreply.github.com>
2023-06-23 17:41:10 +0700

    #10 | Recent Conversations
    Hiển thị các cuộc trò chuyện

--- ChatApp/app/src/main/java/dev/minhtuan07/chatapp/activities/ChatActivity.java
+++ ChatApp/app/src/main/java/dev/minhtuan07/chatapp/activities/ChatActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -38,6 +42,7 @@ public class ChatActivity extends AppCompatActivity {
     private ChatAdapter chatAdapter;
     private PreferenceManager preferenceManager;
     private FirebaseFirestore database;
+    private  String conversionId = null; //mã cuộc trò chuyện
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,7 +73,22 @@ public class ChatActivity extends AppCompatActivity {
         message.put(Constants.KEY_MESSAGE, binding.inputMessage.getText().toString());
         message.put(Constants.KEY_TIMESTAMP, new Date());
         database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
-        binding.inputMessage.setText(null); //reset edit text affter send a message
+        if(conversionId != null){
+            updateConversion(binding.inputMessage.getText().toString());
+        }else { //Nếu chưa từng nhắn tin
+            HashMap<String,Object> conversion = new HashMap<>();
+            conversion.put(Constants.KEY_SENDER_ID,preferenceManager.getString(Constants.KEY_USER_ID));
+            conversion.put(Constants.KEY_SENDER_NAME,preferenceManager.getString(Constants.KEY_NAME));
+            conversion.put(Constants.KEY_SENDER_IMAGE,preferenceManager.getString(Constants.KEY_IMAGE));
+            conversion.put(Constants.KEY_RECEIVER_ID,receiverUser.id);
+            conversion.put(Constants.KEY_RECEIVER_NAME,receiverUser.name);
+            conversion.put(Constants.KEY_RECEIVER_IMAGE,receiverUser.image);
+            conversion.put(Constants.KEY_LAST_MESSAGE,binding.inputMessage.getText().toString());
+            conversion.put(Constants.KEY_TIMESTAMP,new Date());
+            addConversion(conversion);
+
+        }
+        binding.inputMessage.setText(null); //reset edit text after send a message
     }
     private void  listenMessages(){
         database.collection(Constants.KEY_COLLECTION_CHAT)
@@ -110,6 +130,9 @@ public class ChatActivity extends AppCompatActivity {
             binding.chatRecyclerView.setVisibility(View.VISIBLE);
         }
         binding.progressBar.setVisibility(View.GONE);
+        if(conversionId == null){
+            checkForConversion();//kiểm tra xem đã từng nhắn tin hay chưa để tạo mã cuộc trò chuyện
+        }
     };
 
     private Bitmap getBitmapFromEncodedString(String encodedImage){
@@ -128,6 +151,45 @@ public class ChatActivity extends AppCompatActivity {
     private String getReadableDateTime(Date date) {
         return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
+    private void addConversion(HashMap<String,Object> conversion){
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .add(conversion)
+                .addOnSuccessListener(documentReference -> conversionId = documentReference.getId());
+    }
+    private void updateConversion(String message){
+        DocumentReference documentReference=
+                database.collection(Constants.KEY_COLLECTION_CONVERSATIONS).document(conversionId);
+        documentReference.update(
+          Constants.KEY_LAST_MESSAGE,message,
+                Constants.KEY_TIMESTAMP,new Date()
+        );
+    }
+    private void checkForConversion(){
+        if(chatMessages.size() !=0){
+            checkForConversionRemotely(
+                    preferenceManager.getString(Constants.KEY_USER_ID),
+                    receiverUser.id
+            );
+            checkForConversionRemotely(
+                    receiverUser.id,
+                    preferenceManager.getString(Constants.KEY_USER_ID)
+            );
+        }
+    }
+    private  void checkForConversionRemotely(String senderId, String receiverId){
+        database.collection(Constants.KEY_COLLECTION_CONVERSATIONS)
+                .whereEqualTo(Constants.KEY_SENDER_ID,senderId)
+                .whereEqualTo(Constants.KEY_RECEIVER_ID,receiverId)
+                .get()
+                .addOnCompleteListener(conversionOnCompleteListener);
+    }
+
+    private final OnCompleteListener<QuerySnapshot> conversionOnCompleteListener = task ->{
+        if(task.isSuccessful() && task.getResult() != null && task.getResult().getDocuments().size() >0){
+            DocumentSnapshot documentSnapshot = task.getResult().getDocuments().get(0);
+            conversionId= documentSnapshot.getId();
+        }
+    };
 
 
 }
---
+++ ChatApp/app/src/main/java/dev/minhtuan07/chatapp/listeners/ConversionListener.java
@@ -0,0 +1,7 @@
+package dev.minhtuan07.chatapp.listeners;
+
+import dev.minhtuan07.chatapp.models.User;
+
+public interface ConversionListener {
+    void onConversionClicked(User user);
+}
